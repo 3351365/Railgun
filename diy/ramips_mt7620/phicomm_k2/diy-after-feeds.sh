fb973613c46bb3eb89be7f69257081aa4fc386d4
3351365 <89839879+3351365@users.noreply.github.com>
2023-10-08 15:54:43 +0800

    Add files via upload

---
+++ diy/ramips_mt7620/phicomm_k2/diy-after-feeds.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Modify Default IP
+sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
+
+# Modify default hostname
+sed -i 's/ImmortalWrt/K2WRT/g' package/base-files/files/bin/config_generate
+
+# Enable 802.11 r
+#sed -i 's/FtSupport=0/FtSupport=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.2G.dat
+#sed -i 's/FtSupport=0/FtSupport=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
