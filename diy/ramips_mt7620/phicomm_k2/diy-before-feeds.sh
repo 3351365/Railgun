72bacb772b1d0215e463ca3522d0ca9880875c29
3351365 <89839879+3351365@users.noreply.github.com>
2023-10-08 14:43:42 +0800

    Update diy-before-feeds.sh

--- diy/ramips_mt7620/phicomm_k2/diy-before-feeds.sh
+++ diy/ramips_mt7620/phicomm_k2/diy-before-feeds.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
-function git_sparse_clone() {
-    git clone --filter=blob:none --no-checkout --depth=1 $1 $2 && cd $2
-    git sparse-checkout init --cone
-    git sparse-checkout set $3
-    git checkout
-    mv $3 ../$4
-    cd ../ && rm -rf $2
-}
+#
+#function git_sparse_clone() {
+#    git clone --filter=blob:none --no-checkout --depth=1 $1 $2 && cd $2
+#    git sparse-checkout init --cone
+#    git sparse-checkout set $3
+#    git checkout
+#    mv $3 ../$4
+#    cd ../ && rm -rf $2
+#}
 
 # Add mtkiappd support for 802.11 k/v/r
-git_sparse_clone https://github.com/coolsnowwolf/lede lede package/lean/mt/mtkiappd package/kernel/mt-drivers/mtkiappd
+#git_sparse_clone https://github.com/coolsnowwolf/lede lede package/lean/mt/mtkiappd package/kernel/mt-drivers/mtkiappd
